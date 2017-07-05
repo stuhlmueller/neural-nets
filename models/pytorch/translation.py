c6138a5f80d9c40f4b243be4c0cf40bb337e1756
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2017-07-05 11:49:56 -0700

    Added beginnings of PyTorch translation example

---
+++ models/pytorch/translation.py
@@ -0,0 +1,374 @@
+# Based on:
+# - http://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
+# - https://github.com/spro/practical-pytorch/blob/master/seq2seq-translation/seq2seq-translation.ipynb
+
+import unicodedata
+import string
+import re
+import random
+import time
+import math
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch import optim
+import torch.nn.functional as F
+
+USE_CUDA = torch.cuda.is_available()
+print("Using CUDA." if USE_CUDA else "Not using CUDA.")
+
+
+SOS_token = 0
+EOS_token = 1
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2 # Count SOS and EOS
+      
+    def index_words(self, sentence):
+        for word in sentence.split(' '):
+            self.index_word(word)
+
+    def index_word(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+
+# Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+# Lowercase, trim, and remove non-letter characters
+def normalize_string(s):
+    s = unicode_to_ascii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+
+
+def read_langs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    lines = open('/project/data/tatoeba/%s-%s.txt' % (lang1, lang2)).read().strip().split('\n')
+    
+    # Split every line into pairs and normalize
+    pairs = [[normalize_string(s) for s in l.split('\t')] for l in lines]
+    
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+        
+    return input_lang, output_lang, pairs
+
+
+MAX_LENGTH = 10
+
+good_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s",
+    "you are", "you re "
+)
+
+def filter_pair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(' ')) < MAX_LENGTH and \
+        p[1].startswith(good_prefixes)
+
+def filter_pairs(pairs):
+    return [pair for pair in pairs if filter_pair(pair)]
+
+
+
+def prepare_data(lang1_name, lang2_name, reverse=False):
+    input_lang, output_lang, pairs = read_langs(lang1_name, lang2_name, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    
+    pairs = filter_pairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    
+    print("Indexing words...")
+    for pair in pairs:
+        input_lang.index_words(pair[0])
+        output_lang.index_words(pair[1])
+
+    return input_lang, output_lang, pairs
+
+input_lang, output_lang, pairs = prepare_data('eng', 'ger', True)
+
+
+# Return a list of indexes, one for each word in the sentence
+def indexes_from_sentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+def variable_from_sentence(lang, sentence):
+    indexes = indexes_from_sentence(lang, sentence)
+    indexes.append(EOS_token)
+    var = Variable(torch.LongTensor(indexes).view(-1, 1))
+#     print('var =', var)
+    if USE_CUDA: var = var.cuda()
+    return var
+
+def variables_from_pair(pair):
+    input_variable = variable_from_sentence(input_lang, pair[0])
+    target_variable = variable_from_sentence(output_lang, pair[1])
+    return (input_variable, target_variable)
+
+
+class EncoderRNN(nn.Module):
+    
+    def __init__(self, input_size, hidden_size, n_layers=1):
+        super(EncoderRNN, self).__init__()
+        
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        
+    def forward(self, word_inputs, hidden):
+        # Note: we run this all at once (over the whole input sequence)
+        seq_len = len(word_inputs)
+        embedded = self.embedding(word_inputs).view(seq_len, 1, -1)
+        output, hidden = self.gru(embedded, hidden)
+        return output, hidden
+
+    def init_hidden(self):
+        hidden = Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
+        if USE_CUDA: hidden = hidden.cuda()
+        return hidden
+
+    
+class DecoderRNN(nn.Module):
+    
+    def __init__(self, hidden_size, output_size, n_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax()
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        for i in range(self.n_layers):
+            output = F.relu(output)
+            output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        result = Variable(torch.zeros(1, 1, self.hidden_size))
+        if use_cuda:
+            return result.cuda()
+        else:
+            return result
+
+
+teacher_forcing_ratio = 0.5
+clip = 5.0
+
+def train(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+
+    # Zero gradients of both optimizers
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    loss = 0 # Added onto for each word
+
+    # Get size of input and target sentences
+    input_length = input_variable.size()[0]
+    target_length = target_variable.size()[0]
+
+    # Run words through encoder
+    encoder_hidden = encoder.init_hidden()
+    encoder_outputs, encoder_hidden = encoder(input_variable, encoder_hidden)
+    
+    # Prepare input and output variables
+    decoder_input = Variable(torch.LongTensor([[SOS_token]]))
+    decoder_hidden = encoder_hidden # Use last hidden state from encoder to start decoder
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+
+    # Choose whether to use teacher forcing
+    use_teacher_forcing = random.random() < teacher_forcing_ratio
+    if use_teacher_forcing:
+        
+        # Teacher forcing: Use the ground-truth target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            loss += criterion(decoder_output[0], target_variable[di])
+            decoder_input = target_variable[di] # Next target is next input
+
+    else:
+        # Without teacher forcing: use network's own prediction as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+            loss += criterion(decoder_output[0], target_variable[di])
+            
+            # Get most likely word index (highest value) from output
+            topv, topi = decoder_output.data.topk(1)
+            ni = topi[0][0]
+            
+            decoder_input = Variable(torch.LongTensor([[ni]])) # Chosen word is next input
+            if USE_CUDA: decoder_input = decoder_input.cuda()
+
+            # Stop at end of sentence (not necessary when using known targets)
+            if ni == EOS_token: break
+
+    # Backpropagation
+    loss.backward()
+    torch.nn.utils.clip_grad_norm(encoder.parameters(), clip)
+    torch.nn.utils.clip_grad_norm(decoder.parameters(), clip)
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+    
+    return loss.data[0] / target_length
+
+
+def as_minutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def time_since(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
+
+
+hidden_size = 500
+n_layers = 2
+dropout_p = 0.05
+
+# Initialize models
+encoder = EncoderRNN(input_lang.n_words, hidden_size, n_layers)
+decoder = DecoderRNN(hidden_size, output_lang.n_words, n_layers)
+
+# Move models to GPU
+if USE_CUDA:
+    encoder.cuda()
+    decoder.cuda()
+
+# Initialize optimizers and criterion
+learning_rate = 0.0001
+encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+criterion = nn.NLLLoss()
+
+# Configuring training
+n_epochs = 100 # 50000
+plot_every = 200
+print_every = 10 # 200
+
+# Keep track of time elapsed and running averages
+start = time.time()
+plot_losses = []
+print_loss_total = 0 # Reset every print_every
+plot_loss_total = 0 # Reset every plot_every
+
+# Begin!
+for epoch in range(1, n_epochs + 1):
+    
+    # Get training data for this cycle
+    training_pair = variables_from_pair(random.choice(pairs))
+    input_variable = training_pair[0]
+    target_variable = training_pair[1]
+
+    # Run the train function
+    loss = train(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
+
+    # Keep track of loss
+    print_loss_total += loss
+    plot_loss_total += loss
+
+    if epoch == 0: continue
+
+    if epoch % print_every == 0:
+        print_loss_avg = print_loss_total / print_every
+        print_loss_total = 0
+        print_summary = '%s (%d %d%%) %.4f' % (time_since(start, epoch / n_epochs), epoch, epoch / n_epochs * 100, print_loss_avg)
+        print(print_summary)
+
+    if epoch % plot_every == 0:
+        plot_loss_avg = plot_loss_total / plot_every
+        plot_losses.append(plot_loss_avg)
+        plot_loss_total = 0
+
+
+def evaluate(sentence, max_length=MAX_LENGTH):
+    input_variable = variable_from_sentence(input_lang, sentence)
+    input_length = input_variable.size()[0]
+    
+    # Run through encoder
+    encoder_hidden = encoder.init_hidden()
+    encoder_outputs, encoder_hidden = encoder(input_variable, encoder_hidden)
+
+    # Create starting vectors for decoder
+    decoder_input = Variable(torch.LongTensor([[SOS_token]])) # SOS
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+
+    decoder_hidden = encoder_hidden
+    
+    decoded_words = []
+    decoder_attentions = torch.zeros(max_length, max_length)
+    
+    # Run through decoder
+    for di in range(max_length):
+        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+
+        # Choose top word from output
+        topv, topi = decoder_output.data.topk(1)
+        ni = topi[0][0]
+        if ni == EOS_token:
+            decoded_words.append('<EOS>')
+            break
+        else:
+            decoded_words.append(output_lang.index2word[ni])
+            
+        # Next input is chosen word
+        decoder_input = Variable(torch.LongTensor([[ni]]))
+        if USE_CUDA: decoder_input = decoder_input.cuda()
+    
+    return decoded_words
+
+
+def evaluate_randomly():
+    pair = random.choice(pairs)
+    
+    output_words = evaluate(pair[0])
+    output_sentence = ' '.join(output_words)
+    
+    print('>', pair[0])
+    print('=', pair[1])
+    print('<', output_sentence)
+    print('')
+
+
+for n in range(20):
+    evaluate_randomly()
